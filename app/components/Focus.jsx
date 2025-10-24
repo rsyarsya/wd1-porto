3caefb4b3b5ac08ecea7af6c319cb561f7ebab4c
rsyarsya <rsyarsya@gmail.com>
2025-10-24 19:11:52 +0700

    rapiin

--- app/components/Focus.jsx
+++ app/components/Focus.jsx
@@ -4,7 +4,8 @@ import { assets, serviceData } from "@/assets/assets";
 
 const Focus = () => {
   return (
-    <div id="Focus" className="w-full px-[12%] py-10 scroll-mt-20">
+    <div id="Focus" className="w-full px-[12%] py-10
+    scroll-mt-20">
         <h4 className='text-center mb-2 text-lg font-Ovo'>
         What I do</h4>
         <h2 className='text-center text-5xl font-ovo '>
