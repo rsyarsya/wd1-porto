8f475e7eb0cae5b4be621270de7fd571231c44a4
rsyarsya <rasyadwaarsyairnantyanto2006@mail.ugm.ac.id>
2025-10-31 11:26:28 +0700

    tsconfig fix

--- app/(app)/components/Work.jsx
+++ app/(app)/components/Work.jsx
@@ -70,7 +70,7 @@ const Work = ({isDarkMode}) => {
         initial={{ opacity: 0}}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.5, delay:1.1 }}
-        href="" className='w-max flex items-center justify-center gap-2
+        href="https://porto-show-more-1tld.vercel.app/" className='w-max flex items-center justify-center gap-2
         text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto
         my-20 hover:bg-(--color-light-hover) duration-500 dark:text-white dark:border-white
         dark:hover:bg-(--color-dark-hover)'>
